413769de4a87615f45b9fbb274567169e79d969d
Nathaniel Budikey <nthnlbdky916@gmail.com>
2024-06-24 09:30:30 +0800

    Added Portfolio section

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -2,29 +2,9 @@
 import React from "react";
 import { FloatingNav } from "./ui/FloatingNav";
 import { TbHome, TbMessage, TbUser } from "react-icons/tb";
+import { navItems } from "@/data";
+
 export function NavigationBar() {
-  const navItems = [
-    {
-      name: "Home",
-      link: "/",
-      icon: <TbHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
-    },
-    {
-      name: "About",
-      link: "/about",
-      icon: <TbUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
-    },
-    {
-      name: "Portfolio",
-      link: "/portfolio",
-      icon: <TbMessage className="h-4 w-4 text-neutral-500 dark:text-white" />,
-    },
-    {
-      name: "Contact",
-      link: "/contact",
-      icon: <TbMessage className="h-4 w-4 text-neutral-500 dark:text-white" />,
-    },
-  ];
   return (
     <div className="relative  w-full">
       <FloatingNav navItems={navItems} />
