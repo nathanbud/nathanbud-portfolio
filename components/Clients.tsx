7bbacf22baeff43b93c45773d1435fb4503e344f
Nathaniel Budikey <nthnlbdky916@gmail.com>
2024-06-30 18:43:59 +0800

    Added Sentry

---
+++ components/Clients.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Clients = () => {
+  return (
+    <div id="projects" className="py-20">
+      <h1 className="heading">Kind words from</h1>
+      <span className="text-accent-cgreen"> satified clients</span>
+      <div className="flex flex-col items-center"></div>
+    </div>
+  );
+};
+
+export default Clients;
